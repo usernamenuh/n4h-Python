16a15bca9f4e8074651fdb1c97317dda0194e0b7
usernamenuh <muhammadenuh4@gmail.com>
2025-10-17 14:26:48 +0700

    Day 5

--- function.py
+++ function.py
@@ -4,3 +4,12 @@ def nama_function():
 nama_function()
 nama_function()
 nama_function()
+
+def hitung_luas_persegi_panjang(panjang, lebar):
+    luas = panjang * lebar
+    print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}") 
+
+hitung_luas_persegi_panjang(5, 10)
+hitung_luas_persegi_panjang(7, 3)
+hitung_luas_persegi_panjang(8, 4)
+
